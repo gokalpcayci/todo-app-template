30ad41e4503e072163c87dc0dc509a20cb789149
Gokalp Caycı <gklpcayci@gmail.com>
2023-08-29 03:38:45 +0300

    fix: prisma types

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import { DashboardShell } from "@/components/shell"
-import { DashboardHeader } from '@/components/header'
-import { PostCreateButton } from "@/components/post-create-button"
-import { EmptyPlaceholder } from "@/components/empty-placeholder"
+"use client";
+import React from "react";
+import { DashboardShell } from "@/components/shell";
+import { DashboardHeader } from "@/components/header";
+import { PostCreateButton } from "@/components/post-create-button";
+import { EmptyPlaceholder } from "@/components/empty-placeholder";
 
-type Props = {}
-
-export default function page({}: Props) {
-const posts = [""];
+export default function DashboardPage() {
+  const posts = [""];
 
   return (
-   <DashboardShell>
-    <DashboardHeader heading="Todo" text='Create and manage todos.' >
-      <PostCreateButton />
-    </DashboardHeader>
-    <div>
-  
-          <EmptyPlaceholder>
-            <EmptyPlaceholder.Icon name="post" />
-            <EmptyPlaceholder.Title>No todos created</EmptyPlaceholder.Title>
-            <EmptyPlaceholder.Description>
-              You don&apos;t have any todos yet. Start creating content.
-            </EmptyPlaceholder.Description>
-            <PostCreateButton variant="outline" />
-          </EmptyPlaceholder>
-        
+    <DashboardShell>
+      <DashboardHeader heading="Todo" text="Create and manage todos.">
+        <PostCreateButton />
+      </DashboardHeader>
+      <div>
+        <EmptyPlaceholder>
+          <EmptyPlaceholder.Icon name="post" />
+          <EmptyPlaceholder.Title>No todos created</EmptyPlaceholder.Title>
+          <EmptyPlaceholder.Description>
+            You don&apos;t have any todos yet. Start creating content.
+          </EmptyPlaceholder.Description>
+          <PostCreateButton variant="outline" />
+        </EmptyPlaceholder>
       </div>
-
     </DashboardShell>
-  )
+  );
 }
