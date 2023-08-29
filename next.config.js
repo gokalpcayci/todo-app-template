bd8a1c6e36fc490e7e4f0883a73f067b9aa381ad
Gokalp Caycı <gklpcayci@gmail.com>
2023-08-30 00:49:50 +0300

    fix: privacy & terms route bug4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
   experimental: {
     appDir: true,
+    serverComponentsExternalPackages: ["@prisma/client"],
+
   }
 }
 
